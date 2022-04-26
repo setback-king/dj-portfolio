76f55f20410f5974feced91b6c4dbc261bde3ed7
setback-king <90795553+setback-king@users.noreply.github.com>
2022-04-26 16:29:15 -0400

    changes animation per request of client

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -2,7 +2,7 @@ import Projects from "../data/Projects.json"
 import {useState, useEffect} from "react"
 import Project from "./Project"
 import ProjectPage from "./ProjectPage"
-import { click } from "@testing-library/user-event/dist/click"
+
 
 
 const AllProjects = () => {
@@ -11,26 +11,23 @@ const AllProjects = () => {
     const [pageInfo, setPageInfo] = useState([])
 
     const handleClicker = (id) => {
-      handleClickChange(id)
+        setIsClicked(true)
+      document.body.style.overflowY = "hidden"
+      let projectContainer = document.querySelector('.projects--container--wrap')
+      projectContainer.style.filter = 'brightness(60%)'
+      projectContainer.style.pointerEvents = 'none'
+      Projects.map(project => {
+        if(project.id === id) {
+           setPageInfo([{name: project.name, url: project.url, p: project.p, id: project.id, url2: project.url2, url3: project.url3, url4: project.url4, 
+        url5: project.url5, url6: project.url6, url7: project.url7, url8: project.url8, url9: project.url9, url10: project.url10, url11: project.url11, url12: project.url12, url13: project.url13}])
+        }
+      })
+     
     }
 
-    function handleClickChange(id){
-        document.body.style.overflowY = "hidden"
-        let sideBar = document.querySelector('.projectPage')
-        sideBar.style.left = '0';
-        let projectContainer = document.querySelector('.projects--container--wrap')
-        projectContainer.style.filter = 'brightness(60%)'
-        projectContainer.style.pointerEvents = 'none'
-
-        Projects.map(project => {
-            if(project.id === id) {
-               setPageInfo([{name: project.name}])
-            }
-        })
-    }
 
     const handleClose = () => {
-       // setIsClicked(true)
+        setIsClicked(false)
         handleCloseChange()
     }
 
@@ -44,9 +41,25 @@ const AllProjects = () => {
     }
 
     useEffect(() => {
-        setIsClicked(true)
-        setPageInfo([{name: "Kreo"}])
-   
+        setIsClicked(false)
+        setPageInfo([{
+            name: "ALLEN® 50TH ANNIVERSARY",
+            url: "assets/1)Allen50_CoverPhoto.png",
+            p: "The year 2020 marked 50 years of Allen® being in business. We wanted a special logo to promote on our website, apparel, ads, etc. Here you'll see the logo I designed, the product catalog I lead design on, and a few other applications where our team promoted the anniversary. It was promoted effectively at Shot Show, the nation's largest professional event for sport shooting, hunting, and the outdoor industry.",
+            id: 0,
+            url2: "assets/Images_Allen50/1)Logo_Allen50.jpg",
+            url3: "assets/Images_Allen50/2)CatalogCover_InEnvironment_Allen50.jpg",
+            url4: "assets/Images_Allen50/3)CatalogCover_Allen50.jpg",
+            url5: "assets/Images_Allen50/4)CatalogBackCover_Allen50.jpg",
+            url6: "assets/Images_Allen50/5)CatalogSpread1_Allen50.jpg",
+            url7: "assets/Images_Allen50/6)CatalogSpread2_Allen50.jpg",
+            url8: "assets/Images_Allen50/7)CatalogSpread3_Allen50.jpg",
+            url9: "assets/Images_Allen50/8)Tradeshow1_Allen50.jpg",
+            url10: "assets/Images_Allen50/9)Tradeshow2_Allen50.jpg",
+            url11: "assets/Images_Allen50/10)Hangtag_Allen50.jpg",
+            url12: "assets/Images_Allen50/11)SalesAward_Allen50.jpg",
+            url13: "assets/Images_Allen50/12)LogoOptions_InEnvironment_Allen50.jpg",  
+        }])
     }, [])
 
   
--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,26 +1,28 @@
-const ProjectPage = ({handleClose, name, info}) => {
+const ProjectPage = ({handleClose, info}) => {
   
+    const {name, p, url2, url3, url4, url5, url6, url7, url8, url9, url10, url11, url12, url13}=info[0]
+
+
     return (
         <div className="projectPage">
             <div className="project--container">
                 <div className="sideBar--header">
-                    <h1 className='project--header'>{info[0].name}</h1>
+                    <h1 className='project--header'>{name}</h1>
                     <span className="closeSide" onClick={handleClose}>X</span>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing at in tellus integer feugiat scelerisque varius morbi. Risus ultricies tristique nulla aliquet. </p>
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
-                <img className="sideImage" src="assets/2)Allen_CoverPhoto.jpg" alt="" />
+                <p>{p}</p>
+                <img className="sideImage" src={url2} alt="" />
+                <img className="sideImage" src={url3} alt="" />
+                <img className="sideImage" src={url4} alt="" />
+                <img className="sideImage" src={url5} alt="" />
+                <img className="sideImage" src={url6} alt="" />
+                <img className="sideImage" src={url7} alt="" />
+                <img className="sideImage" src={url8} alt="" />
+                <img className="sideImage" src={url9} alt="" />
+                <img className="sideImage" src={url10} alt="" />
+                <img className="sideImage" src={url11} alt="" />
+                <img className="sideImage" src={url12} alt="" />
+                <img className="sideImage" src={url13} alt="" />
 
             </div>
          
