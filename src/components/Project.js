e769b0b4acfb63855e3c8121f632ea341070331a
setback-king <90795553+setback-king@users.noreply.github.com>
2024-05-08 11:17:34 -0400

    fetch priorty main cover photos

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,9 +6,16 @@ const Project = ({ urls, names, id, styles, handleClick, hovImg }) => {
       className="imageBox"
       onClick={(e) => handleClick(e.target.id)}
       style={styles}
+      loading="lazy"
     >
       <img id={id} src={urls} alt={names} className="top" />
-      <img id={id} src={hovImg} alt={names} className="bottom" />
+      <img
+        id={id}
+        src={hovImg}
+        alt={names}
+        className="bottom"
+        fetchpriority="high"
+      />
     </div>
   );
 };
