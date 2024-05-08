7f21c815c1589e4bf851d55faf158027ffa9fd6e
setback-king <90795553+setback-king@users.noreply.github.com>
2024-05-08 10:28:13 -0400

    got rid of margins for mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ const Project = ({ urls, names, id, styles, handleClick, hovImg }) => {
       onClick={(e) => handleClick(e.target.id)}
       style={styles}
     >
-      <img id={id} src={urls} alt={names} className="top" />
+      <img id={id} src={urls} alt={names} className="top" loading="lazy" />
       <img id={id} src={hovImg} alt={names} className="bottom" />
     </div>
   );
